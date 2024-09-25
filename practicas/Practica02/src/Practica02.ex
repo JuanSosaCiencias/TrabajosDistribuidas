e61566cb3be5bedd747ff2a9d9b728fb6fa1a260
JSRMario <juanmario@ciencias.unam.mx>
2024-09-24 18:07:35 -0600

    docs: agrega documentación para la práctica

--- practicas/Practica02/src/Practica02.ex
+++ practicas/Practica02/src/Practica02.ex
@@ -1,41 +1,57 @@
 defmodule Grafica do
   @moduledoc """
   Módulo que representa gráficas con un sistema de mensajes entre procesadores.
-  Selecciona al nodo con menor valor de ID como líder. 
+  Selecciona al nodo con menor valor de ID como líder.
   """
-  
+  @doc """
+  Inicia el proceso de la gráfica.
+
+  ## Parámetros
+  - `estado_inicial` - Estado inicial del proceso, sin visitar sin ID, sin líder y sin vecinos.
+
+  ## Ejemplo
+  ```elixir
+  Grafica.inicia(%{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+  ```
+  """
   def inicia(estado_inicial \\ %{:visitado => false, :id => -1, :lider => nil, :vecinos => []}) do
     recibe_mensaje(estado_inicial)
   end
 
+  @doc """
+  Recibe mensajes y los procesa, utiliza la funcion auxiliar `procesa_mensaje/2`.
+
+  ## Parámetros
+  - `estado` - Estado actual del proceso.
+
+  ## Ejemplo
+  ```elixir
+  Grafica.recibe_mensaje(%{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+  ```
+  """
   def recibe_mensaje(estado) do
     receive do
-      mensaje -> 
-        {:ok, nuevo_estado} = procesa_mensaje(mensaje, estado)      
+      mensaje ->
+        {:ok, nuevo_estado} = procesa_mensaje(mensaje, estado)
       recibe_mensaje(nuevo_estado)
-    end       
-  end  
-
-  # Recibe el ID del proceso y lo almacena en el estado.
+    end
+  end
   def procesa_mensaje({:id, id}, estado) do
     IO.puts("Proceso con PID #{inspect(self())}: Recibí mi ID #{id}")
     estado = Map.put(estado, :id, id)
     {:ok, estado}
-  end  
+  end
 
-  # Recibe la lista de vecinos y lo almacena en el estado.
   def procesa_mensaje({:vecinos, vecinos}, estado) do
     IO.puts("Proceso con PID #{inspect(self())}: Recibí mis vecinos: #{inspect(vecinos)}")
     estado = Map.put(estado, :vecinos, vecinos)
     {:ok, estado}
-  end  
+  end
 
-  # Procesa el mensaje de ID recibido de otro nodo.
   def procesa_mensaje({:mensaje, n_id}, estado) do
     IO.puts("Proceso con PID #{inspect(self())}: Recibí mensaje de mi vecino con ID #{n_id}")
     %{:id => id, :lider => lider_actual} = estado
-    
-    # Si el ID recibido es menor, actualiza el líder y propaga
+
     if n_id < (lider_actual || id) do
       IO.puts("Proceso con ID #{id}: Actualizando líder a #{n_id}")
       estado = Map.put(estado, :lider, n_id)
@@ -47,8 +63,7 @@ defmodule Grafica do
     end
   end
 
-  # Cuando un nodo recibe la señal de inicio, propaga su propio ID a sus vecinos y a sí mismo.
-  def procesa_mensaje({:inicia}, estado) do
+  def procesa_mensaje({:inicia}, estado) do # Cuando un nodo recibe la señal de inicio, propaga su propio ID a sus vecinos y a sí mismo.
     IO.puts("Proceso con PID #{inspect(self())}: Iniciando y propagando mi ID")
     %{:id => id, :vecinos => vecinos, :lider => lider} = estado
 
@@ -59,37 +74,88 @@ defmodule Grafica do
     # Propaga el líder que inicialmente será su propio ID a los vecinos y a sí mismo
     propaga_lider(lider, vecinos, id)
     {:ok, estado}
-  end    
+  end
 
-  # Verifica el líder actual del nodo y lo imprime.
+  @doc """
+  Procesa diferentes tipos de mensajes y actualiza el estado del proceso.
+
+  ## Tipos de mensajes:
+
+  - `{:id, id}`: Recibe un ID y lo asigna al proceso.
+
+    ## Ejemplo
+    ```elixir
+    Grafica.procesa_mensaje({:id, 20}, %{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+    ```
+
+  - `{:vecinos, vecinos}`: Recibe una lista de vecinos y la asigna al proceso.
+
+    ## Ejemplo
+    ```elixir
+    Grafica.procesa_mensaje({:vecinos, [x, w]}, %{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+    ```
+
+  - `{:mensaje, n_id}`: Recibe el ID de un vecino y actualiza el líder si es necesario.
+
+    ## Ejemplo
+    ```elixir
+    Grafica.procesa_mensaje({:mensaje, 20}, %{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+    ```
+
+  - `{:inicia}`: Señal de inicio, propaga su propio ID como el líder a sus vecinos.
+
+    ## Ejemplo
+    ```elixir
+    Grafica.procesa_mensaje({:inicia}, %{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+    ```
+
+  - `{:ya}`: Señal de verificación, imprime el líder actual del proceso.
+
+    ## Ejemplo
+    ```elixir
+    Grafica.procesa_mensaje({:ya}, %{:visitado => false, :id => -1, :lider => nil, :vecinos => []})
+    ```
+  """
   def procesa_mensaje({:ya}, estado) do
     %{:id => id, :lider => lider} = estado
     if lider == id do
       IO.puts("Proceso con ID #{id}: Soy el líder")
-    else 
+    else
       IO.puts("Proceso con ID #{id}: El líder es #{lider}")
     end
     {:ok, estado}
-  end    
+  end
 
-  # Propaga el líder a los vecinos si es el menor ID, y también a sí mismo.
+  @doc """
+  Propaga el líder a los vecinos y a sí mismo.
+
+  ## Parámetros
+  - `nuevo_lider` - ID del nuevo líder.
+  - `vecinos` - Lista de vecinos.
+  - `id` - ID del proceso actual.
+
+  ## Ejemplo
+  ```elixir
+  Grafica.propaga_lider(20, [x, w], 20)
+  ```
+  """
   defp propaga_lider(nuevo_lider, vecinos, id) do
     IO.puts("Proceso con ID #{id}: Propagando el líder #{nuevo_lider} a todos mis vecinos y a mí mismo")
-    
+
     # Incluye al propio proceso en la lista de vecinos
     Enum.each(vecinos ++ [self()], fn vecino -> send(vecino, {:mensaje, nuevo_lider}) end)
-  end        
+  end
 end
 
 # Se crean los procesos y se guardan sus PID.
 
-t = spawn(Grafica, :inicia, [])  
-u = spawn(Grafica, :inicia, [])  
-v = spawn(Grafica, :inicia, [])  
-w = spawn(Grafica, :inicia, [])  
-x = spawn(Grafica, :inicia, [])  
-y = spawn(Grafica, :inicia, [])  
-z = spawn(Grafica, :inicia, [])  
+t = spawn(Grafica, :inicia, [])
+u = spawn(Grafica, :inicia, [])
+v = spawn(Grafica, :inicia, [])
+w = spawn(Grafica, :inicia, [])
+x = spawn(Grafica, :inicia, [])
+y = spawn(Grafica, :inicia, [])
+z = spawn(Grafica, :inicia, [])
 
 # A cada proceso le enviamos su identificador
 send(t, {:id, 20})
@@ -101,9 +167,9 @@ send(y, {:id, 25})
 send(z, {:id, 26})
 
 # A cada proceso le enviamos sus vecinos
-send(t, {:vecinos, [x, w]})  
-send(u, {:vecinos, [y]})    
-send(v, {:vecinos, [x]})    
+send(t, {:vecinos, [x, w]})
+send(u, {:vecinos, [y]})
+send(v, {:vecinos, [x]})
 send(w, {:vecinos, [t, x]})
 send(x, {:vecinos, [t, v, w, y]})
 send(y, {:vecinos, [x, z, u]})
@@ -111,13 +177,13 @@ send(z, {:vecinos, [y]})
 
 # Iniciamos la propagación de forma asíncrona
 
-IO.puts("----------------")  
-IO.puts("Iniciando propagación")  
-IO.puts("----------------")  
+IO.puts("----------------")
+IO.puts("Iniciando propagación")
+IO.puts("----------------")
 
 # Propagación asincrónica: Cada proceso comenzará en orden.
 send(t, {:inicia})
-Process.sleep(1000)  
+Process.sleep(1000)
 send(u, {:inicia})
 Process.sleep(1000)
 send(v, {:inicia})
@@ -132,9 +198,9 @@ send(z, {:inicia})
 
 # Verificación del líder en cada nodo.
 Process.sleep(2000)
-IO.puts("----------------")  
-IO.puts("Verificando elección de líder")  
-IO.puts("----------------")  
+IO.puts("----------------")
+IO.puts("Verificando elección de líder")
+IO.puts("----------------")
 
 send(t, {:ya})
 send(u, {:ya})
