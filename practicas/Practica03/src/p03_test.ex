a03c9a63ed0e598b4ece0a3452e0610a6a192e19
Aldo Angeles <111522210+aaldoaangeles@users.noreply.github.com>
2024-10-25 21:15:14 -0600

    test alterno

--- practicas/Practica03/src/p03_test.ex
+++ practicas/Practica03/src/p03_test.ex
@@ -1,50 +1,32 @@
-# ------------------ Pruebas --------------------------
-
 defmodule Practica03Test do
   use ExUnit.Case
-  # import Practica03
-  alias Practica03, as: P3
-  # alias Grafica, as: G
 
   ExUnit.start()
 
   setup do
     nodos = Practica03.genera(3)
     Enum.each(0..2, fn i -> send(Enum.at(nodos, i), {:id, i}) end)
+    Practica03.send_msg(nodos, {:vecinos, nodos})
     %{nodos: nodos}
   end
 
-  test "todos los nodos se inicializan correctamente", %{nodos: nodos} do
-    assert length(nodos) == 3
-    assert Enum.all?(nodos, &is_pid(&1))
-  end
+  @doc """
+  Prueba de consenso para verificar que todos los nodos acuerden el valor mínimo.
+  """
+  test "todos los nodos llegan a un consenso sobre el valor mínimo", %{nodos: nodos} do
+    # Enviamos valores enteros
+    send(Enum.at(nodos, 0), {:proponer, 8})
+    send(Enum.at(nodos, 1), {:proponer, 4})
+    send(Enum.at(nodos, 2), {:proponer, 1})
 
-
-  test "propagación de valor entre nodos", %{nodos: nodos} do
-    P3.send_msg(nodos, {:vecinos, nodos})
-    send(Enum.at(nodos, 0), {:proponer, "valor_consensuado"})
-
-    # Dar tiempo para que los mensajes se propaguen
+    # Damos tiempo para que los mensajes de consenso se propaguen
     Process.sleep(500)
 
-    # Comprobar que todos los nodos tienen el valor consensuado
-    Enum.each(nodos, fn nodo ->
-      send(nodo, {:comprobar})
-    end)
+    # Verificamos el valor consensuado
+    Practica03.send_msg(nodos, {:comprobar})
     assert true
   end
 
-  test "todos los nodos llegan a un consenso sobre el mismo valor", %{nodos: nodos} do
-    P3.send_msg(nodos, {:vecinos, nodos})
-    send(Enum.at(nodos, 0), {:proponer, "valor_final"})
-
-    # Dar tiempo para la propagación del mensaje
-    Process.sleep(500)
-
-    # Verificar que todos los nodos tienen el valor consensuado
-    Enum.each(nodos, fn nodo ->
-      send(nodo, {:comprobar})
-    end)
-    assert true
-  end
 end
+
+
